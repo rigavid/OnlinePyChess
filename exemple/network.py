72edb7d4e9c3e99a08a0e26c2c570ca08e4033c3
T-Sana <110158900+T-Sana@users.noreply.github.com>
2024-11-02 18:28:06 +0100

    QWERTY

--- exemple/network.py
+++ exemple/network.py
@@ -1,12 +1,11 @@
-from vars import SERVER_PORT
+from vars import SERVER_IP
 import socket
 import pickle
-
 
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = SERVER_PORT
+        self.server = SERVER_IP
         self.port = 5555
         self.addr = (self.server, self.port)
         self.p = self.connect()
@@ -18,12 +17,10 @@ class Network:
         try:
             self.client.connect(self.addr)
             return self.client.recv(2048).decode()
-        except:
-            pass
+        except: pass
 
     def send(self, data):
         try:
             self.client.send(str.encode(data))
             return pickle.loads(self.client.recv(2048*2))
-        except socket.error as e:
-            print(e)
+        except socket.error as e: print(e)
--- morpion/client.py
+++ morpion/client.py
@@ -1,4 +1,24 @@
-from network import Network
-from tsanap import *
-import pickle
+from multiprocessing.connection import Client
+import time
 
+def main():
+    client = Client((input("Server ip: "), input("Server port: ")))
+    id = client.recv()
+    print(f"Connected as {id}")
+    client.send("IMG")
+    img = client.recv()
+    fs = False
+    while True:
+        wk = img.montre(1, fullscreen=fs)
+        if img.is_closed(): break
+        match wk:
+            case 27: break
+            case 32: fs = not fs
+    
+
+if __name__ == "__main__":
+    try: main()
+    except ConnectionRefusedError:
+        print("Server undisponible.")
+    # except EOFError:
+    #     print("Server disconnected.")
